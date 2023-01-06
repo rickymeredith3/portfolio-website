27c9dc3c8874cf889aa702c48d85b565958fd4ab
rmeredith3 <ricky@techuso.com>
2023-01-05 21:32:22 -0700

    Created new Laravel app and installed react

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>{{ config('app.name') }}</title>
+	<script src="https://code.jquery.com/jquery-3.6.3.min.js" integrity="sha256-pvPw+upLPUjgMXY0G+8O0xUf+/Im1MZjXxxgOcBQBXU=" crossorigin="anonymous"></script>
+	<script type="text/javascript" src="{{ mix('/js/app.js') }}"></script>
+	@yield('css')
+	@yield('js')
+</head>
+<body class="page">
+	@yield('content')
+</body>
+</html>
